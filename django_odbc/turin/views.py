e6ae64e728bb8336aedd0a8055d33abae3f6e962
J. Heasly <jheasly@earthlink.net>
2013-07-31 18:57:36 -0700

    I can use this setup. Added .gitignore

--- django_odbc/turin/views.py
+++ django_odbc/turin/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+import mxodbc_django
+import mx.ODBC.Manager as ODBC
 
 def today(request):
-    return render(request, 'turin/base.html')
+    connection = ODBC.DriverConnect('DSN=Dtnews')
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM Category', '')
+    results = cursor.fetchall()
+    cursor.close()
+    return render(request, 'turin/base.html', {'list': results},)
