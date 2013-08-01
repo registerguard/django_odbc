bf53e73b33466b07d38f3fde5cd4e8f60c033383
J. Heasly <jheasly@earthlink.net>
2013-07-31 20:27:56 -0700

    Got it working. A buffer issue though.

--- django_odbc/turin/urls.py
+++ django_odbc/turin/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, include, url
 from turin import views
 
 urlpatterns = patterns('',
-    url(r'^today', views.today, name='local_news'),
+    url(r'^today', views.today, name='index'),
+    url(r'^(?P<storyid>\d{8})', views.getStory, name='detail'),
 )
--- django_odbc/turin/views.py
+++ django_odbc/turin/views.py
@@ -1,11 +1,38 @@
+import datetime
 from django.shortcuts import render
 import mxodbc_django
 import mx.ODBC.Manager as ODBC
 
 def today(request):
+    today = datetime.date.today()
+    eight_digit_date = today.strftime("%Y-%m-%d")
     connection = ODBC.DriverConnect('DSN=Dtnews')
     cursor = connection.cursor()
-    cursor.execute('SELECT * FROM Category', '')
+    cursor.execute('''SELECT sty.storyId, cmsStory.Id, sty.storyname, api.pagesetname, api.letter, api.pagenum, totalDepth, author, origin, subcategoryid, seourl 
+        FROM dbo.addbpageinfo api, dbo.storypageelements spe, dbo.story sty, dt_cms_schema.CMSStory 
+        WHERE api.logicalPageId = spe.logicalPagesId 
+        AND sty.storyId = spe.storyId 
+        AND subCategoryId <> 0 
+        AND NOT api.code = 'TMC' 
+        AND (sty.statusId = 1018 or sty.statusId = 1019 or sty.statusId = 10) 
+        AND cast (rundate as date) = cast ('%s' as date) 
+        AND (numLines > 1 or words > 5) 
+        AND (SELECT sum(isOnLayout) FROM dbo.storyelement WHERE storyid = sty.storyId) > 0 
+        AND story->storyid=sty.storyid 
+        GROUP BY sty.Id ORDER BY api.letter, api.pageNum,totalDepth DESC''' % eight_digit_date, '')
     results = cursor.fetchall()
     cursor.close()
     return render(request, 'turin/base.html', {'list': results},)
+
+def getStory(request, storyid):
+    connection = ODBC.DriverConnect('DSN=Dtnews')
+    cursor = connection.cursor()
+    cursor.execute('''SELECT channelText, xmlTagsId 
+        FROM storyElement 
+        WHERE storyId = %s 
+        AND isOnLayout=1 
+        AND xmltagsid IN (1, 2, 3, 4, 7, 11, 12, 13, 14, 16, 17, 18, 20, 21, 22, 23, 24, 430717) 
+        AND ((SELECT count(*) FROM storyElement WHERE storyId = %s AND isOnLayout=1 AND xmltagsid =3) = 1)''' % (storyid, storyid))
+    results = cursor.fetchall()
+    cursor.close()
+    return render(request, 'turin/story.html', {'detail_list': results},)
