e5565d91c5a95fe245c44031b6553d87fcf5e6da
J. Heasly <jheasly@earthlink.net>
2015-09-11 14:50:14 -0700

    Start cache maker.

---
+++ django_odbc/turin/management/commands/cache_builder.py
@@ -0,0 +1,29 @@
+import mxodbc_django
+import mx.ODBC.Manager as ODBC
+import pprint
+from settings import SUBCATEGORIES_TO_IGNORE
+from django.core.management.base import BaseCommand, CommandError
+
+class Command(BaseCommand):
+    help = u'Requests subcategory indexes pages in order to build a cache.'
+
+    def handle(self, *args, **options):
+        connection = ODBC.DriverConnect('DSN=Dtnews')
+        connection.encoding = 'utf-8'
+        connection.stringformat = ODBC.NATIVE_UNICODE_STRINGFORMAT
+        cursor = connection.cursor()
+
+        self.stdout.write(str(SUBCATEGORIES_TO_IGNORE))
+
+        cursor.execute('''SELECT subCategoryName, subCategoryId FROM SubCategory WHERE subCategoryName NOT IN %s''' % (str(SUBCATEGORIES_TO_IGNORE)))
+        results = cursor.fetchall()
+        for result in results:
+            print result
+
+        cursor.execute('''SELECT COUNT(*) FROM dbo.Story WHERE Story.subCategoryId=6 AND Story.deskId <> 11''')
+        results = cursor.fetchall()
+
+        self.stdout.write('Hi there!')
+        self.stdout.write(str(results))
+
+        cursor.close()
---
+++ django_odbc/turin/management/commands/settings.py
@@ -0,0 +1,6 @@
+SUBCATEGORIES_TO_IGNORE = (
+    'Elaine Zobel RSVP Award',
+    'George Schroeder',
+    'George Schroeder on UO Football',
+    'Not for Web'
+)
