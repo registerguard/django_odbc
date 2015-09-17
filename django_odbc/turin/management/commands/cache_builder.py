89788304b1baa884af81090d9b38c53c42227c9d
J. Heasly <jheasly@earthlink.net>
2015-09-16 18:21:48 -0700

    Now with logging.

--- django_odbc/turin/management/commands/cache_builder.py
+++ django_odbc/turin/management/commands/cache_builder.py
@@ -1,6 +1,10 @@
+import logging
+import logging.handlers
 import mxodbc_django
 import mx.ODBC.Manager as ODBC
+import os
 import pprint
+import requests
 from settings import SUBCATEGORIES_TO_IGNORE
 from django.core.management.base import BaseCommand, CommandError
 
@@ -8,6 +12,17 @@ class Command(BaseCommand):
     help = u'Requests subcategory indexes pages in order to build a cache.'
 
     def handle(self, *args, **options):
+        log_file_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)))
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)-6s: %(name)s - %(levelname)s - %(message)s')
+        handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        fileLogger = logging.handlers.RotatingFileHandler(filename=( log_file_dir + 'cache_builder.log'), maxBytes=256*2048, backupCount=5) # 256x2048 = 512k
+        fileLogger.setFormatter(formatter)
+        logger.addHandler(fileLogger)
+
         connection = ODBC.DriverConnect('DSN=Dtnews')
         connection.encoding = 'utf-8'
         connection.stringformat = ODBC.NATIVE_UNICODE_STRINGFORMAT
@@ -23,14 +38,16 @@ class Command(BaseCommand):
             # [(1,)]
             story_count = cursor.fetchall()
             self.stdout.write('%s stories in SubCategory %s' % (story_count[0][0], result[0]) )
-            subcategory_count_list.append(result.append(list(story_count[0][0])))
-
-
-        # cursor.execute('''SELECT COUNT(*) FROM dbo.Story WHERE Story.subCategoryId=6 AND Story.deskId <> 11''')
-        # results = cursor.fetchall()
-        #
-        # self.stdout.write('Hi there!')
-        # self.stdout.write(str(results))
+            logger.debug('%s stories in SubCategory %s' % (story_count[0][0], result[0]))
+            if int(story_count[0][0]):
+                requests.get('http://registerguard.com/rg/news/categories/?subcats=%s' % result[1])
+                self.stdout.write('Requesting page: http://registerguard.com/rg/news/categories/?subcats=%s' % result[1])
+                logger.debug('Requesting page: http://registerguard.com/rg/news/categories/?subcats=%s' % result[1])
+            else:
+                self.stdout.write('Skipping subcategory %s. %s stories' % (result[0], story_count[0][0]))
+                logger.debug('Skipping subcategory %s. %s stories\n\n' % (result[0], story_count[0][0]))
+            # http://registerguard.com/rg/news/categories/?subcats=
+            # subcategory_count_list.append()
 
         cursor.close()
         pprint.pprint(subcategory_count_list)
